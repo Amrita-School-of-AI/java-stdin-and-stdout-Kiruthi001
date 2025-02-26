5f1b46d807c90372d3199e8b25d1739628cd0eff
Kiruthi001 <kiruthika01052006@gamil.com>
2025-02-26 11:53:20 +0530

    correct_answer

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -9,7 +9,7 @@ public class App {
         int a = scan.nextInt();
         int b = scan.nextInt();
         int c = scan.nextInt();
-        scan.close();
+       //testing code
 
         System.out.println(a);
         System.out.println(b);
